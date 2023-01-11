d767f980bdb6a37247f2ba82822b37fa56368b35
Ainamaani Isaac <aina.isaac2002@gmail.com>
2023-01-11 23:09:56 +0300

    Added more views

--- index/views.py
+++ index/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView,UpdateView
+from django.urls import reverse_lazy
 from . models import Task
 
 # Create your views here.
@@ -14,3 +16,15 @@ class TaskDetail(DetailView):
     model = Task
     template_name = 'task.html'
     context_object_name = 'task'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    template_name = 'taskForm.html'
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    template_name = 'taskForm.html'
